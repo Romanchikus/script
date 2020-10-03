71bcd56c65822c1bb434acdf15710f018d665ab7
Romanchikus <13ternopil@gmail.com>
2020-10-03 19:14:00 +0300

    Finally

--- Roman_Obednyak.py
+++ Roman_Obednyak.py
@@ -1,7 +1,11 @@
+""" Please, enter parameters to console like in examples: 
+    -l https://github.com/fastlane/fastlane -s 2020-08-09T00:00:00Z
+    -l https://github.com/fastlane/fastlane -s 2020-01-09T00:00:00Z -u 'Sep 30 2020 at 7:40AM' """
+
+import argparse
 import requests
 import dateutil, datetime
 import dateutil.parser as parser
-from pandas import pandas as pd
 
 
 def investigate(
@@ -10,109 +14,107 @@ def investigate(
     link="https://github.com/fastlane/fastlane",
     branch="master",
 ):
-    owner, repo = link.split("/")[3:5]
+    owner, repo = link.split("/")[3:]
     since_url, until_url = time_formater(since, until)
-    print("Searching in period: since={} until={}".format(since, until))
+    print(
+        "\x1b[6;31;47m"
+        + "Search throughout: since={} until={}".format(since, until, "blue")
+        + " on github.com repository "
+        + link
+        + "\x1b[0m"
+    )
 
     url_commits = "https://api.github.com/repos/{0}/{1}/commits?sha={2}{3}{4}&page=1&per_page=100".format(
         owner, repo, branch, since_url, until_url
     )
 
     rate_user = rate_user_commits(url_commits)
-    print('     ==Rate of commits==     \n',rate_user)
+    print("\x1b[3;30;44m" + "==Rate of commits==", "\x1b[0m")
+    print_table(rate_user)
 
     url = "https://api.github.com/search/issues?page=1&per_page=1&q=is:pr+repo:{0}/{1}+base:{2}".format(
         owner, repo, branch
     )
     num_open_pr, num_closed_pr = get_num_of_tags(url, until, since)
-    print("Quantity of PR: open->", num_open_pr, "closed->", num_closed_pr)
+    print(
+        "\x1b[1;32;40m" + "Quantity of PR: open->",
+        num_open_pr,
+        "closed->",
+        num_closed_pr,
+        "\x1b[0m",
+    )
 
     url = "https://api.github.com/search/issues?page=1&per_page=1&q=is:pr+repo:{0}/{1}+base:{2}".format(
         owner, repo, branch
     )
     old_RP = num_of_old_tags(url)
-    print("Quantity of 'old' PR ->", old_RP)
+    print("\x1b[1;32;40m" + "Quantity of 'old' PR ->", old_RP, "\x1b[0m")
 
     url = "https://api.github.com/search/issues?page=1&per_page=1&q=is:issue+repo:{0}/{1}".format(
         owner, repo
     )
     num_open_iss, num_closed_iss = get_num_of_tags(url, until, since)
-    print("Quantity of issues: open->", num_open_iss, "closed->", num_closed_iss)
+    print(
+        "\x1b[1;32;40m" + "Quantity of issues: open->",
+        num_open_iss,
+        "closed->",
+        num_closed_iss,
+        "\x1b[0m",
+    )
 
     url = "https://api.github.com/search/issues?page=1&per_page=1&q=is:issue+repo:{0}/{1}".format(
         owner, repo
     )
     old_iss = num_of_old_tags(url, days=14)
-    print("Quantity of 'old' issues ->", old_iss)
+    print("Quantity of 'old' issues ->", "\x1b[2;30;41m", old_iss, "\x1b[0m")
+
 
 def get_num_of_tags(url, until, since):
     if until and since:
         num_sinc_open_pr, num_sinc_closed_pr = start_from_time(
-            url=url, time_period=since,
+            url=url,
+            time_period=since,
         )
         num_unt_open_pr, num_unt_closed_pr = start_from_time(
-            url=url, time_period=until,
+            url=url,
+            time_period=until,
         )
-        num_open_pr, num_closed_pr = (num_sinc_open_pr-num_unt_open_pr),(num_sinc_closed_pr-num_unt_closed_pr)
+        num_open_pr, num_closed_pr = (num_sinc_open_pr - num_unt_open_pr), (
+            num_sinc_closed_pr - num_unt_closed_pr
+        )
     elif since:
-        num_open_pr, num_closed_pr = start_from_time(
-            url=url, time_period=since
-        )
+        num_open_pr, num_closed_pr = start_from_time(url=url, time_period=since)
     elif until:
         num_open_pr, num_closed_pr = start_from_time(
             url=url, time_period=until, created="+created:<{}"
         )
     elif not (until and since):
         num_open_pr = get_num__pr(url)
-        num_closed_pr = get_num__pr(url, condition='+is:closed')
+        num_closed_pr = get_num__pr(url, condition="+is:closed")
     return num_open_pr, num_closed_pr
 
 
 def num_of_old_tags(url, days=30):
-    time_period = parser.parse(str(datetime.datetime.now())).date() - dateutil.relativedelta.relativedelta(days=days)
+    time_period = parser.parse(
+        str(datetime.datetime.now())
+    ).date() - dateutil.relativedelta.relativedelta(days=days)
     url = url + "+created:<{}".format(time_period)
-    print(url)
     num_open_pr = get_num__pr(url)
     return num_open_pr
-
-
-# def num_PR(until, since, branch, owner, repo):
-#     url = "https://api.github.com/search/issues?page=1&per_page=1&q=is:pr+repo:{0}/{1}+base:{2}".format(
-#         owner, repo, branch
-#     )
-#     if until and since:
-#         num_sinc_open_pr, num_sinc_closed_pr = start_from_time(
-#             url=url, time_period=since,
-#         )
-#         num_unt_open_pr, num_unt_closed_pr = start_from_time(
-#             url=url, time_period=until,
-#         )
-#         num_open_pr, num_closed_pr = (num_sinc_open_pr-num_unt_open_pr),(num_sinc_closed_pr-num_unt_closed_pr)
-#     elif since:
-#         num_open_pr, num_closed_pr = start_from_time(
-#             url=url, time_period=since
-#         )
-#     elif until:
-#         num_open_pr, num_closed_pr = start_from_time(
-#             url=url, time_period=until, created="+created:<{}"
-#         )
-#     elif not (until and since):
-#         num_open_pr = get_num__pr(url)
-#         num_closed_pr = get_num__pr(url, condition='+is:closed')
-#     return num_open_pr, num_closed_pr
 
 
 def start_from_time(url, time_period, created="+created:>{}"):
     time_period = parser.parse(time_period).date()
     url = url + created.format(time_period)
     num_open_pr = get_num__pr(url)
-    num_closed_pr = get_num__pr(url, condition='+is:closed')
-    print('url', url)
+    num_closed_pr = get_num__pr(url, condition="+is:closed")
     return num_open_pr, num_closed_pr
 
-def get_num__pr(url, condition='+is:open'):
+
+def get_num__pr(url, condition="+is:open"):
     num_pr = requests.get(url + condition).json()["total_count"]
     return num_pr
+
 
 def rate_user_commits(url):
     loop = True
@@ -128,7 +130,6 @@ def rate_user_commits(url):
     top_users = sorted(users.items(), key=lambda x: x[1], reverse=True)
     if len(top_users) >= 30:
         top_users = top_users[:30]
-    top_users = pd.DataFrame(top_users, columns=["author", "commits"])
     return top_users
 
 
@@ -144,6 +145,12 @@ def make_rate(load_json, users):
     return users
 
 
+def print_table(list_users):
+    for i, d in enumerate(list_users):
+        line = "|".join(str(x).ljust(16) for x in d)
+        print(line)
+
+
 def time_formater(since, until):
     if since:
         since = "&since=" + parser.parse(since).isoformat()
@@ -156,31 +163,20 @@ def time_formater(since, until):
     return since, until
 
 
-def num_pages(url):
-
-    url = url.split("&")
-    del url[url.index("per_page=1")]
-
-    for s in url:
-        if "page" in s:
-            pages = s.split("page=")
-
-    return int(-(-int(pages[1]) // 100))
-
 if __name__ == "__main__":
-    print("Enter date since")
-    since = "2020-01-09T00:00:00Z"
-    print("Enter date until")
-    until = "Sep 30 2020 at 7:40AM"
-    investigate( since=since, link='https://github.com/fastlane/fastlane')
-    # investigate(until, since)
-
-class Bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    parse_obj = argparse.ArgumentParser(description="Git script")
+    parse_obj.add_argument(
+        "-l",
+        action="store",
+        type=str,
+        dest="link",
+        default="https://github.com/fastlane/fastlane",
+    )
+    parse_obj.add_argument("-s", action="store", type=str, dest="since", default=None)
+    parse_obj.add_argument("-u", action="store", type=str, dest="until", default=None)
+    parse_obj.add_argument(
+        "-b", action="store", type=str, dest="branch", default="master"
+    )
+    args = parse_obj.parse_args()
+    link, since, until, branch = args.link, args.since, args.until, args.branch
+    investigate(link=link, since=since, until=until, branch=branch)
